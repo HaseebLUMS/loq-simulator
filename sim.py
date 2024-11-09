1b7bd69a6c0345b9bd65e57a7d526a68f033bf5b
HaseebLUMS <mh6218@nyu.edu>
2024-11-09 20:13:17 +0000

    added plan for the simulator

---
+++ sim.py
@@ -0,0 +1,14 @@
+def main():
+    # Create a sequence of orders, containing several prices, and traders
+
+    # Feed them to a ME, which maintains an LOB and processes the sequence, and get output O denoting which order got matched with what other order
+
+    # Also feed the sequence to a LOQ simulator, which reorders the sequence emulating how LOQ would do it
+
+    # Feed then reordered sequence to ME and get the output O'
+
+    # Compare O to O'
+    pass
+
+if __name__ == "__main__":
+    main()
